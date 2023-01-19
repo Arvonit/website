de84dfcd4f13470d4fb551a25b1d9de0ef377d83
Arvind <arvind@kasiliya.com>
2023-01-19 00:33:25 -0500

    Force Inter font to be fetched before rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter', display: 'swap' });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
