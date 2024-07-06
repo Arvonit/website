759ee49e598359be0afb8e51650813e3b20810a7
Arvind Kasiliya <arvind@kasiliya.com>
2024-07-06 13:24:32 -0400

    Update about section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,26 +6,28 @@ import ExternalLink from './external-link';
 export default function About() {
   return (
     <div>
-      <h2 className="text-2xl font-semibold mb-4 dark:text-white">About Me</h2>
+      <h2 className="text-2xl font-semibold mb-4 dark:text-white">Welcome!</h2>
 
-      <div className="flex flex-col md:flex-row md:space-x-8 md:items-start items-center">
+      <div className="flex flex-col md:flex-row md:space-x-8 items-center">
         <div className="basis-2/3 order-2 md:order-1 prose dark:prose-invert">
           {/* Bio */}
           <p>
             Hey, I&apos;m Arvind! I recently graduated from the University of Connecticut where I
             studied Computer Science with a concentration in cybersecurity. In my free time, I like
             to play the guitar, play tennis, watch TV, hang out, and occasionally{' '}
-            <Link href="/blog">write</Link>.
+            <Link href="/blog">write</Link>. Welcome to my home on the internet!
           </p>
           <p>
             In the past, I worked as a cybersecurity research assistant where I developed{' '}
             <Link href="https://bgpy.engr.uconn.edu">BGPsy</Link>, an interactive web app to
             simulate the Border Gateway Protocol. Last summer, I interned at Travelers where I
             worked on an internal app portal for accessing different QlikSense dashboards using
-            React and AWS. Feel free to check out my work on{' '}
-            <Link href="https://github.com/Arvonit">GitHub</Link> and connect with me on{' '}
-            <Link href="https://www.linkedin.com/in/arvindkasiliya">LinkedIn</Link>. I&apos;m always
-            down to chat!
+            React and AWS.
+          </p>
+          <p>
+            Below, I&apos;ve curated a selection of my best projects. Feel free to connect with me
+            on <Link href="https://www.linkedin.com/in/arvindkasiliya">LinkedIn</Link> and{' '}
+            <Link href="https://github.com/Arvonit">GitHub</Link>. I&apos;m always down to chat!
           </p>
 
           {/* Links */}
--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -9,8 +9,8 @@ export default function ExternalLink() {
       className="stroke-sky-600 dark:stroke-sky-400 fill-none size-5 rounded-lg stroke-2"
       viewBox="0 0 26 26"
       // stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round">
+      strokeLinecap="round"
+      strokeLinejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
       <path d="M11 13l9 -9" />
